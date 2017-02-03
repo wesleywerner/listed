9ace9edde63b6fd466da8ac082b5427e307329e3
wesley <wesley.werner@gmail.com>
2017-02-03 19:35:46 +0000

    Add tests for addItem.

--- test/listed.test.js
+++ test/listed.test.js
@@ -1,34 +1,25 @@
 // make available to cli unit tests
 if (typeof require != 'undefined') {
   var assert = require('chai').assert;
+  var expect = require('chai').expect;
   var Listed = require('../source/js/listed.js');
 }
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-
 describe('Listed Test Suite', function() {
 
-  describe('molly', function() {
-    var data = {};
+  describe('methods', function() {
+    
     beforeEach(function() {
-      data = Listed.getData();
+      Listed.data.list = [];
     });
-    it('modifies the data', function() {
-      data.tail = true;
-      return true;
+    
+    it('add a list item', function() {
+      Listed.methods.addItem('test item');
+      expect(Listed.data.list).to.have.lengthOf(1);
+      expect(Listed.data.list[0].text).to.equal('test item');
+      expect(Listed.data.list[0].checked).to.be.false;
     });
-    it('should be named molly', function() {
-      assert.equal(data.name, 'molly');
-    });
-    it('should not have a tail', function() {
-      assert.equal(data.tail, false);
-    });
+
   });
   
   describe('storage', function() {
