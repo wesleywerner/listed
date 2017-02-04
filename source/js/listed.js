70eeee2b99d69f14ffce7f86697ab9aa40c06cf5
Wesley <wesley.werner@gmail.com>
2017-02-04 06:51:45 +0200

    Add methods and unit tests: addItem, amendItem, removeItem, findItemAt.

--- source/js/listed.js
+++ source/js/listed.js
@@ -8,9 +8,34 @@ Listed.data.newItemText = 'new item';
 // method context is scoped via Vue so that 'this' references the Listed.data object
 Listed.methods = {};
 
-// Add a list item
+
+Listed.methods.findItemAt = function (text) {
+  var idx = Listed.data.list.findIndex( function(n) { 
+    return n.text == text 
+  });
+  return idx;
+}
+
 Listed.methods.addItem = function (text) {
-  Listed.data.list.push({'text':text, 'checked': false});
+  if (Listed.methods.findItemAt(text) == -1) {
+    Listed.data.list.push({'text':text, 'checked': false});
+  }
+}
+
+Listed.methods.removeItem = function (text) {
+  var idx = Listed.methods.findItemAt(text);
+  if (idx > -1) {
+    Listed.data.list.splice(idx, 1);
+  }
+}
+
+Listed.methods.amendItem = function (text, amendment) {
+  amendment = amendment || '';
+  if (amendment.trim() == '') return;
+  var idx = Listed.methods.findItemAt(text);
+  if (idx > -1) {
+    Listed.data.list[idx].text = amendment;
+  }
 }
 
 // make available to cli unit tests
--- test/listed.test.js
+++ test/listed.test.js
@@ -7,22 +7,91 @@ if (typeof require != 'undefined') {
 
 describe('Listed Test Suite', function() {
 
-  describe('methods', function() {
+  describe('shopping list management methods', function() {
     
     beforeEach(function() {
       Listed.data.list = [];
     });
     
-    it('add a list item', function() {
+    it('should find an item', function() {
+      Listed.data.list = [{'text':'item 1'},{'text':'item 2'},{'text':'item 3'}];
+      var idx = Listed.methods.findItemAt('item 2');
+      expect(idx).to.be.equal(1);
+    });
+    
+    it('should add a new item', function() {
       Listed.methods.addItem('test item');
       expect(Listed.data.list).to.have.lengthOf(1);
       expect(Listed.data.list[0].text).to.equal('test item');
       expect(Listed.data.list[0].checked).to.be.false;
     });
 
+    it('should not add a duplicate item', function() {
+      Listed.methods.addItem('test item');
+      Listed.methods.addItem('test item');
+      expect(Listed.data.list).to.have.lengthOf(1);
+      expect(Listed.data.list[0].text).to.equal('test item');
+      expect(Listed.data.list[0].checked).to.be.false;
+    });
+    
+    it('should remove an item', function() {
+      Listed.methods.addItem('test item 1');
+      Listed.methods.addItem('test item 2');
+      Listed.methods.addItem('test item 3');
+      Listed.methods.removeItem('test item 2');
+      // test length
+      expect(Listed.data.list).to.have.lengthOf(2);
+      // test correct item got removed
+      var idx = Listed.methods.findItemAt('test item 2');
+      expect(idx).to.equal(-1);
+    });
+
+    it('should amend an item', function() {
+      Listed.methods.addItem('test item 1');
+      Listed.methods.addItem('test item 2');
+      Listed.methods.addItem('test item 3');
+      Listed.methods.amendItem('test item 2', 'test item n');
+      var idx = Listed.methods.findItemAt('test item n');
+      expect(idx).to.equal(1);
+    });
+
+    it('should not amend to empty item', function() {
+      Listed.methods.addItem('test item 1');
+      Listed.methods.addItem('test item 2');
+      Listed.methods.addItem('test item 3');
+      Listed.methods.amendItem('test item 2', null);
+      Listed.methods.amendItem('test item 2', undefined);
+      Listed.methods.amendItem('test item 2', '');
+      Listed.methods.amendItem('test item 2', ' ');
+      var idx = Listed.methods.findItemAt('test item 2');
+      expect(idx).to.equal(1);
+    });
+
+  });
+
+  describe('history list management methods', function() {
+    
+    var history = Listed.data.history;
+    
+    beforeEach(function() {
+      Listed.data.history = [];
+    });
+    
+    it.skip('should add a new item', function() {
+      
+    });
+
+    it.skip('should not add a duplicate item', function() {
+      
+    });
+
+    it.skip('should amend an item', function() {
+      
+    });
+
   });
   
-  describe('storage', function() {
+  describe('local storage methods', function() {
     var iscli = typeof localforage == 'undefined';
       it('save to local storage', function(){
           if (iscli) {
