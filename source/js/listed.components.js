cac5a6d6c1a4117409ccfc2d2ca428375f6d589a
Wesley <wesley.werner@gmail.com>
2017-02-05 08:30:13 +0200

    Use component events to remove items.

--- source/js/listed.components.js
+++ source/js/listed.components.js
@@ -1,4 +1,9 @@
 Vue.component('list-item', {
   props: ['item'],
-  template: '<li><label><input type="checkbox" id="checkbox" v-model="item.checked">{{ item.text }}</label></li>'
+  template: '<li><label><input type="checkbox" id="checkbox" v-model="item.checked">{{ item.text }}</label> <button v-on:click="remove">X</button> </li>',
+  methods: {
+    remove: function() {
+      this.$emit('remove', this.item.text);
+    }
+  }
 })
