c15bd4f2759301f9f57f2bc67a982295827fd94e
Wesley <wesley.werner@gmail.com>
2017-02-05 09:21:06 +0200

    Create a history autocomplete input.

--- source/js/listed.components.js
+++ source/js/listed.components.js
@@ -7,3 +7,33 @@ Vue.component('list-item', {
     }
   }
 })
+
+/**
+ * A text input that auto completes history items.
+ * Props:
+ *    id: uniquely identity this component.
+ *    history: the history items to auto complete.
+ *    auto-clear: if "true" the input will clear after input key enter.
+ * Events:
+ *    selected: fired when the enter key is used on the input.
+ */
+Vue.component('item-autocomplete', {
+  props: ['id', 'history', 'autoClear'],
+  data: function() {
+    return {
+      inputValue: ''
+    }
+  },
+  template: '<div> <input v-bind:list="id" v-model="inputValue" v-on:keyup.enter="selected" /> \
+            <datalist v-bind:id="id"> \
+              <option v-for="item in history" v-bind:value="item.text"> \
+            </datalist> </div>',
+  methods: {
+    selected: function() {
+      this.$emit('selected', this.inputValue);
+      if (this.autoClear == 'true') {
+        this.inputValue = '';
+      }
+    }
+  }
+})
