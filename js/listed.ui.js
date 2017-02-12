213598b35b64f2a574531e5ab646d85644688292
Wesley <wesley.werner@gmail.com>
2017-02-12 18:06:26 +0200

    Build

--- js/listed.ui.js
+++ js/listed.ui.js
@@ -1,3 +1,19 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+
 /**
  * Seperation of conerns for UI specific methods to attach to the Vue instance.
  */
@@ -51,6 +67,7 @@
   ui.promptCleanList = function () {
     if (confirm('Clean checked items from your list?')) {
       Listed.methods.cleanList();
+      Listed.data.saved = false;
     }
   }
   
