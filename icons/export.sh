146d9d2619ab7cacf8cfcd5ec1c048d1af24a751
Wesley <wesley.werner@gmail.com>
2017-02-23 07:01:28 +0200

    Add android icons.

---
+++ icons/export.sh
@@ -0,0 +1,6 @@
+inkscape --export-png android/ldpi.png -w 36 android/ic_shopping_cart_black_24px.svg
+inkscape --export-png android/mdpi.png -w 48 android/ic_shopping_cart_black_24px.svg
+inkscape --export-png android/hdpi.png -w 72 android/ic_shopping_cart_black_24px.svg
+inkscape --export-png android/xhdpi.png -w 96 android/ic_shopping_cart_black_24px.svg
+inkscape --export-png android/xxhdpi.png -w 144 android/ic_shopping_cart_black_24px.svg
+inkscape --export-png android/xxxhdpi.png -w 192 android/ic_shopping_cart_black_24px.svg
